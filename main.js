961adf18a14460f2a3097dbd509501394bcca1b0
Bhagyeshpro <78053878+Bhagyeshpro@users.noreply.github.com>
2021-06-24 03:42:10 +0530

    er

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
+// Input block Names Assignment
 const celciusInput = document.getElementById('celcius');
 const farhenheitInput = document.getElementById('fahrenheit');
 const kelvinInput = document.getElementById('kelvin');
 
+// Get Input Fron Input Block And Store it in Variable
 const inputs = document.getElementsByClassName('inputs');
 
+// For loop Depends on Input
 for (let i = 0; i < inputs.length; i++) {
     let input = inputs[i];
 
     input.addEventListener("input", function (e) {
+
+        // ParseFloat used to convert value from String to Number in JS
         let userNumber = parseFloat(e.target.value);
 
+        // Change Multiple Input Block Using Few Statement
         switch (e.target.name) {
             case "celcius":
+                console.log(typeof userNumber);
                 farhenheitInput.value = (userNumber + 1.8) + 32; 
                 kelvinInput.value = userNumber + 273.1;
                 break;
@@ -25,8 +32,6 @@ for (let i = 0; i < inputs.length; i++) {
                 celciusInput.value = userNumber - 273.15;
                 farhenheitInput.value = ((userNumber - 273.15) * 1.8) + 32;
                 break;
-
-
         }
 
     });
